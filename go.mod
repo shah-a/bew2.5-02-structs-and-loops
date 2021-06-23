976dff7b7f6fd23ea4610844524cfe03c4b82bc9
Ali Shah <s.ashah@outlook.com>
2021-06-23 05:51:56 -0400

    [Finished] challenge
    Notes:
    - I had some trouble figuring out how to get a string input from the
      keyboard including the spaces. I'm not a fan of the ReadString
      way, because it requires some clunky-ish code to remove the newline.
      I found another way from Stack Overflow which I implemented here.
      This new method uses the `Scan()` method of a bufio `Scanner` instead
      of a `Reader`.

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/shah-a/bew2.5-02-structs-and-loops
-
-go 1.16
--- main/main.go
+++ main/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	_ "log"
+	"log"
+	"os"
 )
 
 type House struct {
@@ -12,10 +14,75 @@ type House struct {
 	price    float64
 }
 
+var reader = bufio.NewReader(os.Stdin)
+var scanner = bufio.NewScanner(os.Stdin)
+
 func main() {
-	rooms := 5
-	address := "123 Blah St"
-	city := "Blahville"
-	price := 123456.0
-	fmt.Printf("%s\t%s\t%d Rooms \t$%.0f\n", address, city, rooms, price)
+	var houses []House
+
+	for {
+		// add is used to decide when to terminate house-appending loop
+		if !add() {
+			fmt.Println() // For terminal spacing
+			break
+		}
+
+		fmt.Println() // For terminal spacing
+
+		var address, city string
+		var numRooms int
+		var price float64
+
+		// Stack Overflow solution to getting input with spaces:
+		fmt.Print("Enter address -> ")
+		if scanner.Scan() {
+			address = scanner.Text()
+		}
+
+		fmt.Print("Enter city -> ")
+		if scanner.Scan() {
+			city = scanner.Text()
+		}
+
+		// fmt.Print("Enter address -> ")
+		// fmt.Fscanf(reader, "%q", &address)
+
+		// fmt.Print("Enter city -> ")
+		// fmt.Fscanf(reader, "%q", &city)
+
+		fmt.Print("Enter number of rooms -> ")
+		if _, err := fmt.Fscanln(reader, &numRooms); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print("Enter price -> ")
+		if _, err := fmt.Fscanln(reader, &price); err != nil {
+			log.Fatal(err)
+		}
+
+		houses = append(houses, House{address, city, numRooms, price})
+
+		fmt.Println() // For terminal spacing
+	}
+
+	for i := 0; i < len(houses); i++ {
+		fmt.Printf("%s\t\t%s\t\t%d Rooms\t\t$%.2f\n", houses[i].address, houses[i].city, houses[i].numRooms, houses[i].price)
+	}
+}
+
+// add is used to decide when to terminate house-appending loop
+func add() bool {
+	var add string
+
+	fmt.Print("Add a new house? (y/n) -> ")
+
+	if _, err := fmt.Fscanln(reader, &add); err != nil {
+		log.Fatal(err)
+	}
+
+	if add == "n" {
+		return false
+	}
+
+	return true
 }
